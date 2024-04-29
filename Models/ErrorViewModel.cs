a5ed5e89e8d79572c7889d4f8d487b1c499ab72c
Bipuldeshar1 <desharbipul@gmail.com>
2024-04-29 15:17:32 +0545

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace jobPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
