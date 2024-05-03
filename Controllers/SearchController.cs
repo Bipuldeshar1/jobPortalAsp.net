ea59d399e43d1f586fe7078a6184bd0e3b5525aa
Bipuldeshar1 <desharbipul@gmail.com>
2024-05-03 13:36:31 +0545

    search added

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using jobPortal.data;
+using jobPortal.Models.ViewModel.Search;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace jobPortal.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext context;
+
+        public SearchController(ApplicationDbContext context)
         {
+            this.context = context;
+        }
+        public async Task<IActionResult> SearchJob(string? JobTitle, string? CompanyName)
+        {
+            if (!string.IsNullOrEmpty(JobTitle) && string.IsNullOrEmpty(CompanyName))
+            {
+                var jobs = context.JobModels.Where(x => x.Title.Contains(JobTitle)).ToList();
+                return View(jobs);
+            }
+            else if (!string.IsNullOrEmpty(CompanyName) && string.IsNullOrEmpty(JobTitle))
+            {
+                var companies = context.AppUsers.Where(x => x.Name.Contains(CompanyName)).ToList();
+                return View("CompanySearch",companies);
+            }
+            else if (!string.IsNullOrEmpty(JobTitle) || !string.IsNullOrEmpty(CompanyName))
+            {
+                var jobs = context.JobModels.Include(x => x.appUser).AsQueryable();
+
+                if (!string.IsNullOrEmpty(CompanyName))
+                {
+                    jobs = jobs.Where(x => x.appUser.Name.Contains(CompanyName));
+                }
+
+                if (!string.IsNullOrEmpty(JobTitle))
+                {
+                    jobs = jobs.Where(x => x.Title.Contains(JobTitle));
+                }
+
+                return View(await jobs.ToListAsync());
+            }
+
             return View();
         }
+
     }
 }
---
+++ Models/ViewModel/Search/searchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using jobPortal.Models.job;
+
+namespace jobPortal.Models.ViewModel.Search
+{
+    public class searchViewModel
+    {
+        public List<AppUser> appUser { get; set; }
+        public List<JobModel> jobModel {  get; set; }
+    }
+}
